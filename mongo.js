c5b2cfc8054d08effc0fed12e4561d8d3265c23a
AndreiTotoro <andreitotoro@gmail.com>
2022-08-23 15:34:19 +0300

    Add abiulity to display all the entries in the database and add new ones from terminal

--- mongo.js
+++ mongo.js
@@ -12,17 +12,35 @@ const personSchema = mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema);
 
-mongoose
-	.connect(url)
-	.then((result) => {
-		const person = new Person({
-			id: 1,
-			name: 'Andrei',
-			number: '123456789',
+if (process.argv.length === 5) {
+	mongoose
+		.connect(url)
+		.then((result) => {
+			const person = new Person({
+				id: Math.floor(Math.random() * 10000 + 1),
+				name: process.argv[3],
+				number: process.argv[4],
+			});
+			console.log(
+				`Added ${person.name} number ${person.number} to the phonebook!"`
+			);
+			return person.save();
+		})
+		.then(() => {
+			return mongoose.connection.close();
 		});
-
-		return person.save();
-	})
-	.then(() => {
-		return mongoose.connection.close();
+} else if (process.argv.length === 3) {
+	mongoose.connect(url).then(() => {
+		Person.find({}).then((result) => {
+			console.log('Phonebook:');
+			result.forEach((person) => {
+				console.log(`${person.name} ${person.number}`);
+			});
+			mongoose.connection.close();
+		});
 	});
+} else {
+	console.log(
+		'Invalid number or arguments! Please provide either just your password or your password and the name and phone number of the person you want to add to the phonebook!'
+	);
+}
